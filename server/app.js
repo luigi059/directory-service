3a557cc2610130162a6414e6c860567d076a62ae
Luigi Fernandez <luigimarkfernandez@gmail.com>
2020-12-27 16:08:53 +0000

    designing database

--- server/app.js
+++ server/app.js
@@ -1,37 +1,26 @@
-const express = require('express');
-const cors = require('cors');
-
-const viewRouter = require('./routes/viewRoutes');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 // Create a new express application named 'app'
 const app = express();
-
 // Set our backend port to be either an environment variable or port 5000
 const port = process.env.PORT || 5000;
-
 // This application level middleware prints incoming requests to the servers console, useful to see incoming requests
 app.use((req, res, next) => {
     console.log(`Request_Endpoint: ${req.method} ${req.url}`);
     next();
 });
-
-/* // Configure the bodyParser middleware
+// parse requests of content-type - application/json
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-})); */
-
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 // Configure the CORs middleware
 app.use(cors());
 
-// Routes
-app.use('/home',viewRouter);
-
-// Catch any bad requests
-app.get('*', (req, res) => {
-    res.status(200).json({
-        msg: 'Catch All'
-    });
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to the backend!" });
 });
 
 // Configure our server to listen on the port defiend by our port variable
